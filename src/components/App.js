087e3ff21f6f8d4b4eed2c926ae68d84d8a98f19
slizbeth <stefany.lizbeth@gmail.com>
2020-01-13 16:24:43 +0100

    filter of characteres with input name

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import '../stylesheets/App.css';
 import {fetchCharacters} from '../services/api.js'
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allCharacteres: []
+      allCharacteres: [],
+      value: ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange(evt){
+    this.setState({
+      value: evt.target.value
+    })
+  }
+  
   componentDidMount() {
     fetchCharacters()
     .then(data => {
@@ -19,12 +29,18 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className="App">
-        Rick & Morty Search
+        <form>
+            <label>
+                <input type="text" onChange={this.handleChange} name="character" value={this.state.value}/>
+            </label>
+        </form>
         <ul>
-          {this.state.allCharacteres.map((character) => <li>
+          {this.state.allCharacteres
+          .filter(character => this.state.value === '' || character.name.toLowerCase()
+          .includes(this.state.value.toLowerCase()))
+          .map((character) => <li key={character.id}>
             <img src={character.image} alt={character.name}/>
             <h4>{character.name}</h4>
             <p>{character.species}</p>
