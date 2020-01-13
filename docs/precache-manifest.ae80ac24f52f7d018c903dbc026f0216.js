0afe819e4c0bb1049db74c6c81165d0770b6d2f7
slizbeth <stefany.lizbeth@gmail.com>
2020-01-13 15:30:12 +0100

    add react structure

---
+++ docs/precache-manifest.ae80ac24f52f7d018c903dbc026f0216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa68c31cd54a47e56f7bfd31cbb661f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aea751d245be2fedc695",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c3f386b17b463aa8ce42",
+    "url": "./static/js/2.a0132573.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.a0132573.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aea751d245be2fedc695",
+    "url": "./static/js/main.3365a3f1.chunk.js"
+  },
+  {
+    "revision": "985e89fa39f96252f4dd",
+    "url": "./static/js/runtime-main.b43b4e21.js"
+  }
+]);
---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../stylesheets/App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        Rick & Morty Search
+      </div>
+    );
+  }
+}
+
+export default App;
