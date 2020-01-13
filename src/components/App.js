4ebc86d8d13c4ce367153d0b3ae1496a64b0f712
slizbeth <stefany.lizbeth@gmail.com>
2020-01-13 15:45:24 +0100

    add fetch api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import '../stylesheets/App.css';
-
+import {fetchCharacters} from '../services/api.js'
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allCharacteres: []
+    };
+  }
+
+  componentDidMount() {
+    fetchCharacters()
+    .then(data => {
+      this.setState({
+        allCharacteres: data.results
+      })
+   })
+  }
 
   render() {
+    console.log(this.state);
     return (
       <div className="App">
         Rick & Morty Search
